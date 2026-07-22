ae6fd67301ba6c70211ad3a05af179c342cf3374
Pradeep D <pradeepdinakaran13@gmail.com>
2026-07-22 10:21:20 +0530

    Prepare project for deployment

--- server/server.js
+++ server/server.js
@@ -14,16 +14,21 @@ const wishlistRoutes = require("./routes/wishlistRoutes");
 const notificationRoutes = require("./routes/notificationRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 
-const invoiceRoutes =
-  require("./routes/invoiceRoutes");
+const invoiceRoutes = require("./routes/invoiceRoutes");
 
 const connectDB = require("./config/db");
 connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
@@ -33,8 +38,8 @@ app.use("/api/wishlist", wishlistRoutes);
 
 app.use("/api/upload", uploadRoutes);
 
-app.use("/api/invoice",invoiceRoutes);
-app.use("/api/notifications",notificationRoutes);
+app.use("/api/invoice", invoiceRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.use("/api/payment", paymentRoutes);
 app.use("/api/cart", cartRoutes);
