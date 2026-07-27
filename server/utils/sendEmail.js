8b06df65eb20ad508af19d3c7f516a7fa2b9c324
Pradeep D <pradeepdinakaran13@gmail.com>
2026-07-27 13:53:55 +0530

    fix code

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,14 +1,16 @@
 const nodemailer = require("nodemailer");
 
-const nodemailer = require("nodemailer");
-
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
